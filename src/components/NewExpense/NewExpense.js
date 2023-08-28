d48d0f8e81bb7c797ad443b896b1982843cd8301
home <home@bradshaw.local>
2023-08-28 20:46:14 +0100

    Show and hide New Expense Form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,39 @@
-import React from "react"
+import React, {useState} from "react"
 
 import ExpenseForm from "./ExpenseForm"
 import "./NewExpense.css"
 
 const NewExpense = (props) => {
+  const [isFormShowing, setIsFormShowing] = useState(false)
+
   const saveExpenseDataHandler = (entertedExpenseData) => {
     const expenseData = {
       ...entertedExpenseData,
       id: Math.random().toString()
     }
     props.onAddExpense(expenseData)
+    setIsFormShowing(false)
+  }
+
+  const startEditingHandler = () => {
+    setIsFormShowing(true)
+  }
+
+  const stopEditingHandler = () => {
+    setIsFormShowing(false)
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {!isFormShowing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isFormShowing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   )
 }
